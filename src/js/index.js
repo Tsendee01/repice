ac772b27bd99c0c6aa12887b9ecd840f3e6d17f0
tsendee <tsendeee01@gmail.com>
2022-08-03 13:01:30 +0800

    dos=cs folder uusgelee git server ashiglahiin tuld

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,6 @@
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+
+import Search from './model/Search';
+
+let search = new Search('pasta');
+
+search.doSearch().then(r => console.log(r));
